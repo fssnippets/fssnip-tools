656447572dbae6175c4f22f751f0221324830f32
tpetricek <tomas@tomasp.net>
2015-06-30 00:15:23 +0100

    Adding restore and generate scripts

---
+++ format.fsx
@@ -0,0 +1,38 @@
+﻿#r "packages/FSharp.Data/lib/net40/FSharp.Data.dll"
+#r "packages/FsPickler/lib/net40/FsPickler.dll"
+#r "packages/Paket/tools/paket.exe"
+#load "packages/FSharp.Formatting/FSharp.Formatting.fsx"
+open System
+open System.IO
+open FSharp.Literate
+open FSharp.CodeFormat
+open Nessos.FsPickler
+open FSharp.Data
+
+type Index = JsonProvider<"data/index.json">
+let snippets = Index.GetSample().Snippets
+let data = __SOURCE_DIRECTORY__ + "/data"
+
+let formatAgent = CodeFormat.CreateAgent()
+let pickler = FsPickler.CreateXml()
+
+let formatSnippet (parsed:string) (formatted:string) source nuget = 
+  let doc = Literate.ParseScriptString(source, "/Snippet.fsx", formatAgent)  
+  let html = Literate.WriteHtml(doc, "fs", true, true)
+  File.WriteAllText(formatted, html)
+  use wr = new StreamWriter(parsed)
+  pickler.Serialize(wr, doc)  
+
+for s in snippets do 
+  printfn "Processing snippet: %d" s.Id
+  for v in 0 .. s.Versions - 1 do
+    let source = File.ReadAllText(sprintf "%s/source/%d/%d" data s.Id v)
+    let dir = sprintf "%s/parsed/%d" data s.Id 
+    if not (Directory.Exists(dir)) then Directory.CreateDirectory(dir) |> ignore
+    let dir = sprintf "%s/formatted/%d" data s.Id 
+    if not (Directory.Exists(dir)) then Directory.CreateDirectory(dir) |> ignore
+    let formattedFile = sprintf "%s/formatted/%d/%d" data s.Id v
+    let parsedFile = sprintf "%s/parsed/%d/%d" data s.Id v
+    if not (File.Exists(formattedFile)) || not (File.Exists(parsedFile)) then
+      formatSnippet parsedFile formattedFile source s.References
+
---
+++ load.fsx
@@ -0,0 +1,79 @@
+﻿#r "packages/SQLProvider/lib/net40/FSharp.Data.SqlProvider.dll"
+open System
+open System.IO
+open FSharp.Data.Sql
+
+// ------------------------------------------------------------------------------------------------
+
+let alphabet = [ '0' .. '9' ] @ [ 'a' .. 'z' ] @ [ 'A' .. 'Z' ] |> Array.ofList
+let alphabetMap = Seq.zip alphabet [ 0 .. alphabet.Length - 1 ] |> dict
+
+/// Generate mangled name to be used as part of the URL
+let mangleId i =
+  let rec mangle acc = function
+    | 0 -> new String(acc |> Array.ofList)
+    | n -> let d, r = Math.DivRem(n, alphabet.Length)
+           mangle (alphabet.[r]::acc) d
+  mangle [] i
+
+/// Translate mangled URL name to a numeric snippet ID
+let demangleId (str:string) = 
+  let rec demangle acc = function
+    | [] -> acc
+    | x::xs -> 
+      let v = alphabetMap.[x]
+      demangle (acc * alphabet.Length + v) xs
+  demangle 0 (str |> List.ofSeq)
+
+// ------------------------------------------------------------------------------------------------
+
+type FsSnip = 
+  SqlDataProvider<"Data Source=.\\SQLExpress;Initial catalog=fssnip_data;Integrated security=true",
+                  DatabaseVendor = Common.DatabaseProviderTypes.MSSQLSERVER>
+
+let ctx = FsSnip.GetDataContext()
+
+let versions = 
+  [ for v in ctx.``[dbo].[Versions]`` -> 
+      v.CodeID, v.VersionNumber, (v.Formatted, v.Source) ]
+  |> Seq.groupBy (fun (id, vv, data) -> id)
+  |> dict
+
+let EnsureDirectory s = 
+  if not (Directory.Exists(s)) then Directory.CreateDirectory(s) |> ignore
+
+let data = __SOURCE_DIRECTORY__ + "/data"
+
+for c in ctx.``[dbo].[Code]`` do
+  if not (versions.ContainsKey(c.ID)) then
+    printfn "Failed for id %d (%s)" c.ID (mangleId c.ID)
+  else
+    EnsureDirectory(data + "/formatted/" + string c.ID) |> ignore
+    EnsureDirectory(data + "/source/" + string c.ID) |> ignore
+    for _, version, (formatted, source) in versions.[c.ID] do
+      File.WriteAllText(data + "/formatted/" + string c.ID + "/" + (string version), formatted)
+      File.WriteAllText(data + "/source/" + string c.ID + "/" + (string version), source)
+
+#r "packages/FSharp.Data/lib/net40/FSharp.Data.dll"
+open FSharp.Data
+
+type Snippets = JsonProvider<"""{"snippets":
+ [{ "id": 0, "title": "Sample", "comment": "Sample", "author": "Sample", "link": "Sample",
+    "date": "2015-06-11T23:39:43.6454087-04:00", "likes": 3, "isPrivate": true, "passcode": "fsharp",
+    "references": ["FSharp","Data"], "source": "fun3d", "versions": 3, "tags": ["fsharp", "sample"] },
+  { "id": 0, "title": "Sample", "comment": "Sample", "author": "Sample", "link": "Sample",
+    "date": "2015-06-11T23:39:43.6454087-04:00", "likes": 3, "isPrivate": true, "passcode": "fsharp",
+    "references": ["FSharp","Data"], "source": "fun3d", "versions": 3, "tags": ["fsharp", "sample"] }] }""">
+
+let snippets = 
+  [| for i in ctx.``[dbo].[Code]`` ->
+      let refs = i.References.Split([|','|], StringSplitOptions.RemoveEmptyEntries) |> Array.map (fun s -> s.Trim())
+      Snippets.Snippet
+        ( id = i.ID, title = i.Title, comment = i.Comment, author = i.Author, link = i.Link,
+          date = i.Date, likes = i.Likes, isPrivate = i.Private, passcode = "", // i.Passcode,
+          references = refs, source = i.Source, versions = Seq.length i.FK_Versions_Code,
+          tags = [| for t in i.FK_CodeTags_Code -> t.Tag |] ) |]
+
+File.WriteAllText(data + "/index.json", Snippets.Root(snippets).JsonValue.ToString())
+
+
