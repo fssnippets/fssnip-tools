b84d1ba2aa7a8233d4135c85b918039a6bfac621
Tomas Petricek <tomas@tomasp.net>
2021-09-27 20:26:41 +0100

    Add download script

---
+++ download.fsx
@@ -0,0 +1,10 @@
+﻿#load "packages/FSharp.Azure.StorageTypeProvider/StorageTypeProvider.fsx"
+#load "config.fsx"
+open FSharp.Azure.StorageTypeProvider
+open System.IO
+
+type Azure = AzureTypeProvider<Config.azureConnectionString>
+
+let target = Path.Combine(__SOURCE_DIRECTORY__, "data")
+let op = Azure.Containers.data.Download(target)
+Async.RunSynchronously(op)
